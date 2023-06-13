1dca7018c1623c28d9b138a69bf18d5dd14a5eab
TrarbachArthur <arthurtrarbach.sampaio@gmail.com>
2023-06-13 10:28:21 -0300

    :sparkles: implement stack

--- src/ed/stack.c
+++ src/ed/stack.c
@@ -1,4 +1,31 @@
-
+#include <stdlib.h>
+#include "deque.h"
 #include "queue.h"
 
-// TODO!
+struct Stack {
+    Deque* deque;
+};
+
+Stack *stack_construct() {
+    Stack* stack = calloc(1, sizeof(Stack));
+    stack->deque = deque_construct();
+
+    return stack;
+}
+
+void stack_push(Stack *stack, void *data) {
+    deque_push_front(stack->deque, data);
+}
+
+bool stack_empty(Stack *stack) {
+    return (deque_size(stack->deque) == 0);
+}
+
+void *stack_pop(Stack *stack) {
+    deque_pop_front(stack->deque);
+}
+
+void stack_destroy(Stack *stack) {
+    deque_destroy(stack->deque);
+    free(stack);
+}
