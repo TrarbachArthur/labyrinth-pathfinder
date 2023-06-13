8252001698ebd79db9f72a4a56e137a050ea292b
TrarbachArthur <arthurtrarbach.sampaio@gmail.com>
2023-06-13 10:38:42 -0300

    :sparkles: implement queue

--- src/ed/queue.c
+++ src/ed/queue.c
@@ -1,4 +1,31 @@
-
+#include <stdlib.h>
+#include "deque.h"
 #include "queue.h"
 
-// TODO!
+struct Queue {
+    Deque* deque;
+};
+
+Queue *queue_construct() {
+    Queue *queue = calloc(1, sizeof(Queue));
+    queue->deque = deque_construct(deque);
+
+    return queue;
+}
+
+void queue_push(Queue *queue, void *data) {
+    deque_push_back(queue->deque, data);
+}
+
+bool queue_empty(Queue *queue) {
+    return (deque_size(queue->deque) == 0);
+}
+
+void *queue_pop(Queue *queue) {
+    return deque_pop_front(queue->deque);
+}
+
+void queue_destroy(Queue *queue) {
+    deque_destroy(queue->deque);
+    free(queue);
+}
