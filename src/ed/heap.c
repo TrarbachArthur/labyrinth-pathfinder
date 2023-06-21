5bb346e90965f36180acd0c694087e29196c9157
TrarbachArthur <arthurtrarbach.sampaio@gmail.com>
2023-06-21 12:04:14 -0300

    :boom: finish heap implementation

--- src/ed/heap.c
+++ src/ed/heap.c
@@ -44,7 +44,29 @@ void heapifyUp(Heap *heap) {
     heap->nodes[pos] = aux;
 }
 
-void heapifyDown(Heap *heap);
+void heapifyDown(Heap *heap) {
+    int pos = 0, index = 0;
+    Node aux = heap->nodes[pos];
+
+    while (pos < (heap->size)/2) {
+        int left = (pos*2) + 1;
+        int right = (pos*2) + 2;
+        index = left;
+        if (right < heap->size) {
+            if (heap->nodes[left].priority < heap->nodes[right].priority) {
+                index = right;
+            }
+        }
+        if (aux.priority >= heap->nodes[index].priority) break;
+
+        printf("Troquei %d e %d\n", pos, index);
+
+        heap->nodes[pos] = heap->nodes[index];
+        pos = index; 
+    }
+
+    heap->nodes[pos] = aux;
+}
 
 void heap_push(Heap *heap, void *data, double priority) {
     if (heap->size >= heap->capacity) {
@@ -58,13 +80,39 @@ void heap_push(Heap *heap, void *data, double priority) {
     heapifyUp(heap);
     heap->size++;
 
-    //heap_print(heap);
+    heap_print(heap);
 }
 
-bool heap_empty(Heap *heap);
-void *heap_max(Heap *heap);
-double heap_max_priority(Heap *heap);
-void *heap_pop(Heap *heap);
+bool heap_empty(Heap *heap) {
+    return (!heap->size);
+}
+
+void *heap_max(Heap *heap) {
+    return heap->nodes[0].data;
+}
+
+double heap_max_priority(Heap *heap) {
+    return heap->nodes[0].priority;
+}
+
+void *heap_pop(Heap *heap) {
+    if (heap->size <= 0) {
+        printf("Heap is empty!\n");
+        return NULL;
+    }
+
+    void *data = heap->nodes[0].data;
+    // Setting last element as first for heapify
+    heap->nodes[0] = heap->nodes[heap->size - 1];
+    heap->size--;
+
+    heapifyDown(heap);
+
+    heap_print(heap);
+
+    return data;
+}
+
 void heap_destroy(Heap *heap) {
     for (int i = 0; (i < heap->size) && heap->nodes[i].data; i++) {
         free(heap->nodes[i].data);
