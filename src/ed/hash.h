e20a7211c1c813768a455e05c663455114451afb
TrarbachArthur <arthurtrarbach.sampaio@gmail.com>
2023-06-21 12:29:19 -0300

    :bricks: change hash.h

--- src/ed/hash.h
+++ src/ed/hash.h
@@ -4,25 +4,59 @@
 
 typedef struct HashTable HashTable;
 
-// criacao da tabela hash
-HashTable *hash_table_construct();
+typedef int (*HashFunction)(HashTable *, void *);
+typedef int (*CmpFunction)(void *k1, void *k2);
+
+typedef struct
+{
+    void *key;
+    void *val;
+} HashTableItem;
+
+typedef struct HashTableIterator HashTableIterator;
+
+/**
+ * @brief Cria a tabela hash
+ * @param table_size
+ * Número de buckets na tabela hash
+ * @param hash_fn
+ * Ponteiro de função que recebe como entrada uma chave do tipo void* e retorna um inteiro
+ * @param cmp_fn
+ * Ponteiro de função que recebe como entrada dois void* representando duas chaves e tem a mesma semântica da função strcmp
+ * @return HashTable*
+ * Retorna o ponteiro da tabela hash recém alocada.
+ */
+HashTable *hash_table_construct(int table_size, HashFunction hash_fn, CmpFunction cmp_fn);
 
 // funcao para insercao/atualizacao de pares chave-valor em O(1).
-// Eh responsabilidade do usuario gerar a chave como um inteiro.
-// internamente a tabela hash realiza o hash modular para colocar o elemento na posicao correta
-// lista encadeada sao usadas para tratar colisoes
-void hash_table_set(HashTable *h, int key, void *val);
+// Se a chave ja existir, atualiza o valor e retorna o valor antigo para permitir desalocacao.
+void *hash_table_set(HashTable *h, void *key, void *val);
 
-// retorna o valor associado com a chave key ou NULL se ela nao existir
-void *hash_table_get(HashTable *h, int key);
+// retorna o valor associado com a chave key ou NULL se ela nao existir em O(1).
+void *hash_table_get(HashTable *h, void *key);
 
-// remove o par chave-valor e retorna o valor ou NULL se nao existir tal chave
-void *hash_table_pop(HashTable *h, int key);
+// remove o par chave-valor e retorna o valor ou NULL se nao existir tal chave em O(1).
+void *hash_table_pop(HashTable *h, void *key);
 
-// numero de elementos
+// numero de buckets
 int hash_table_size(HashTable *h);
+
+// numero de elementos inseridos
+int hash_table_num_elems(HashTable *h);
 
 // libera o espaco alocado para a tabela hash
 void hash_table_destroy(HashTable *h);
+
+// cria um novo iterador para a tabela hash
+HashTableIterator *hash_table_iterator(HashTable *h);
+
+// retorna 1 se o iterador chegou ao fim da tabela hash ou 0 caso contrario
+int hash_table_iterator_is_over(HashTableIterator *it);
+
+// retorna o proximo par chave valor da tabela hash
+HashTableItem *hash_table_iterator_next(HashTableIterator *it);
+
+// desaloca o iterador da tabela hash
+void hash_table_iterator_destroy(HashTableIterator *it);
 
 #endif
